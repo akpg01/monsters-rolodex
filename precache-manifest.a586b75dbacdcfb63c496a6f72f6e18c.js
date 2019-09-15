d83ce5fab733b81f8beafe08946d271f2730469f
Grace Akpan <opps4lrng@gmail.com>
2019-09-14 20:54:17 -0600

    Updates

---
+++ precache-manifest.a586b75dbacdcfb63c496a6f72f6e18c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8716430a26b3ea3039ba9c9093d06f4e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "06bb6f13fa48c67bcb1a",
+    "url": "/monsters-rolodex/static/css/main.f95f940f.chunk.css"
+  },
+  {
+    "revision": "4b7594f9ba93f757104a",
+    "url": "/monsters-rolodex/static/js/2.765d0f25.chunk.js"
+  },
+  {
+    "revision": "06bb6f13fa48c67bcb1a",
+    "url": "/monsters-rolodex/static/js/main.d2d65a81.chunk.js"
+  },
+  {
+    "revision": "f7fff3448ef51b7cb626",
+    "url": "/monsters-rolodex/static/js/runtime~main.edd7f36b.js"
+  }
+]);
